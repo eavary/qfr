931df93d97baaafa525aa708739dc2b6b8a92470
Beckett Avary <beckettav@gmail.com>
2024-08-16 14:36:10 -0700

    struct definitions for schedule representation

--- esp/main/qfr_skd.h
+++ esp/main/qfr_skd.h
@@ -1,20 +1,27 @@
 
+#pragma once
+
 #include <stdint.h>
 
-typedef int32_t qfr_time_t;
+#define QFR_GET_DAY(x) ((x & 0xe0000000) >> 29)
+#define QFR_GET_DUR(x) (x & 0x1fffffff)
 
-//typedef struct {
-//    int32_t zone_id;
-//    qfr_time_t time;
-//    int32_t duration;
-//} qfr_block_t;
+// minutes since sunday midnight
+typedef uint32_t qfr_time_t;
+
+typedef struct qfr_skd_op_t {
+    qfr_time_t time;
+    uint32_t duration;
+} qfr_skd_op_t;
+
 
 typedef struct qfr_skd_t {
-    int32_t* zone_ids;
-    qfr_time_t* times;
-    int32_t* durations;
+    qfr_skd_op_t* operations;
+    uint32_t* zone_ids;
 } qfr_skd_t;
 
 typedef struct qfr_skd_cb_t {
+    uint16_t on_min;
+    uint16_t off_min;
 } qfr_skd_cb_t;
 
