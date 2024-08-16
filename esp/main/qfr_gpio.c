b23170e519cbd8216c631496a284aeef09346643
Beckett Avary <beckettav@gmail.com>
2024-08-16 16:28:46 -0700

    init gpio for led

---
+++ esp/main/qfr_gpio.c
@@ -0,0 +1,16 @@
+
+#include <qfr_gpio.h>
+
+#include <esp_err.h>
+#include <driver/gpio.h>
+
+void qfr_gpio_init(void) {
+
+}
+
+void qfr_gpio_led_on(void) {
+}
+
+void qfr_gpio_led_off(void) {
+}
+
---
+++ esp/main/qfr_gpio.h
@@ -0,0 +1,6 @@
+
+void qfr_gpio_init(void);
+
+void qfr_gpio_led_on(void);
+void qfr_gpio_led_off(void);
+
