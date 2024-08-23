17c33ec71612586dbedf4ccf7310cd6ad48db737
Beckett Avary <beckettav@gmail.com>
2024-08-23 01:46:44 -0700

    flesh out scheduling api

--- esp/main/qfr_skd.c
+++ esp/main/qfr_skd.c
@@ -6,30 +6,48 @@
 #include <esp_log.h>
 #include <esp_timer.h>
 
+#include <string.h>
+
 #define MIN_TO_US(min) (uint64_t)(min*60000000)
 #define DAY_US MIN_TO_US(24*60)
 
-// using task dispatch
+// time,duration,zone_id
+void qfr_skd_from_csv(qfr_skd_t* skd, char* csv, size_t csv_len) {
+    size_t num_lines = 0;
+    for (size_t i = 0; i < csv_len; i++) if (csv[i] == '\n') num_lines++;
 
+    skd->times = calloc(num_lines, sizeof (qfr_time_t));
+    skd->durs = calloc(num_lines, sizeof (uint32_t));
+    skd->ids = calloc(num_lines, sizeof (uint32_t));
 
-void qfr_skd_daily_on_cb(void) {
+    char* tok = strtok(csv, ",\n");
+    size_t num_tok = 0;
+    size_t idx;
+    while (tok) {
+        idx = num_tok/3;
+        switch (num_tok%3) {
+        case 0:
+            skd->times[idx] = atoi(tok);
+            break;
+        case 1:
+            skd->durs[idx] = atoi(tok);
+            break;
+        case 2:
+            skd->ids[idx] = atoi(tok);
+            break;
+        }
+        num_tok++;
+        tok = strtok(NULL, ",\n");
+    }
 }
 
-void qfr_skd_daily_off_cb(void) {
+void qfr_skd_reg_timers(qfr_skd_t skd) {
+    //TODO
 }
 
-// expects `timers` on stack of size `sz`
-static void get_time(qfr_skd_op_t* ops, size_t sz, qfr_skd_cb_t* times) {
-}
-
-void qfr_skd_oneshot_on_cb(void) {
-    // register qfr_skd_daily_cb
-}
-
-void qfr_skd_oneshot_off_cb(void) {
-}
-
-void qfr_skd_init(void) {
-    // get schedule from fs, attempt sync
+void qfr_skd_free(qfr_skd_t* skd) {
+    free(skd->times);
+    free(skd->durs);
+    free(skd->ids);
 }
 
--- esp/main/qfr_skd.h
+++ esp/main/qfr_skd.h
@@ -2,23 +2,27 @@
 #pragma once
 
 #include <stdint.h>
+#include <stddef.h>
 
 // minutes since sunday midnight
 typedef uint32_t qfr_time_t;
 
-typedef struct qfr_skd_op_t {
-    qfr_time_t time;
-    uint32_t duration;
-} qfr_skd_op_t;
-
-
+// arrays are malloc'd
 typedef struct qfr_skd_t {
-    qfr_skd_op_t* operations;
-    uint32_t* zone_ids;
+    qfr_time_t* times;
+    uint32_t* durs;
+    uint32_t* ids;
+    size_t len;
 } qfr_skd_t;
 
 typedef struct qfr_skd_cb_t {
     uint16_t on_min;
     uint16_t off_min;
 } qfr_skd_cb_t;
+
+void qfr_skd_from_csv(qfr_skd_t* skd, char* csv, size_t csv_len);
+
+void qfr_skd_reg_timers(qfr_skd_t skd);
+
+void qfr_skd_free(qfr_skd_t* skd);
 
