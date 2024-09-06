104aaf574019780a8d02aa8fccd80dadc34047de
Eric Avary <eavary@mac.com>
2024-09-06 13:22:24 -0700

    QFRModal: common modal for device operations (#12)

---
+++ frontend/src/components/device/AddDeviceButton.tsx
@@ -0,0 +1,93 @@
+import { useRef, useState } from 'react'
+import {
+  FormControl,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react'
+import { AddIcon } from '@chakra-ui/icons'
+import type { Device } from '../../types/device'
+
+import QFRModal from '../QFRModal'
+
+interface ModalProps {
+  onSubmitted: (device: Device) => void
+}
+
+const AddDeviceButton = ({ onSubmitted }: ModalProps) => {
+  const modal = useRef()
+
+  const [name, setName] = useState('')
+  const [ipAddress, setIPAddress] = useState('')
+  const [hostname, setHostname] = useState('')
+
+  const resetForm = () => {
+    setName('')
+    setIPAddress('')
+    setHostname('')
+  }
+
+  const processSubmit = (event: any) => {    
+    event.preventDefault()
+
+    if (name.length > 0 && ipAddress.length > 0 && hostname.length > 0) {
+      onSubmitted({
+        name: name,
+        ip_address: ipAddress,
+        hostname: hostname
+      })
+      handleClose()
+    }
+  }
+
+  const handleClose = () => {
+    resetForm()
+    modal.current.close()
+  }
+
+  const handleOpen = () => {
+    modal.current.open()
+  }
+
+  return (
+    <>
+      <AddIcon _hover={{cursor: 'pointer'}} onClick={handleOpen}></AddIcon>
+      <QFRModal 
+        ref={modal}
+        confirmText='Submit'
+        title="Add Device"
+        cancelText="Cancel"
+        onModalClose={handleClose}
+        onConfirmed={processSubmit}
+      >
+        <form id="add-device-form" onSubmit={processSubmit}>
+          <FormControl my={2}>
+            <FormLabel>Device Name</FormLabel>
+            <Input 
+              placeholder="Name"
+              value={name}
+              onChange={e => setName(e.currentTarget.value)}
+            />
+          </FormControl>
+          <FormControl my={2}>
+            <FormLabel>Hostname</FormLabel>
+            <Input 
+              placeholder="Hostname"                   
+              value={hostname}
+              onChange={e => setHostname(e.currentTarget.value)}
+            />
+          </FormControl>
+          <FormControl my={2}>
+            <FormLabel>IP Address</FormLabel>
+            <Input 
+              placeholder="IP Address"
+              value={ipAddress}
+              onChange={e => setIPAddress(e.currentTarget.value)}
+            />
+          </FormControl>
+        </form>
+      </QFRModal>
+    </>
+  )
+}
+
+export default AddDeviceButton
