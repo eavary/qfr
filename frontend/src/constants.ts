0fd7aec1db6ffa85dcf72f5116fee919595cdf19
Eric Avary <eavary@mac.com>
2024-09-24 18:48:55 -0700

    [schedule] constant for API_URL

---
+++ frontend/src/constants.ts
@@ -0,0 +1,10 @@
+export const API_URL = 'http://localhost:3000/api'
+
+export const DAY_OF_WEEK = [
+  { id: 1, name: 'Sunday',    value: 'Su' },
+  { id: 2, name: 'Monday',    value: 'Mo' },
+  { id: 3, name: 'Tuesday',   value: 'Tu' },
+  { id: 4, name: 'Wednesday', value: 'We' },
+  { id: 5, name: 'Thursday',  value: 'Th' },
+  { id: 6, name: 'Friday',    value: 'Fr' },
+]
